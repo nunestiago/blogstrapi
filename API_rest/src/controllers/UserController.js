0c327aadc3b45a27f8bf03d5e85fb9d3b6eb33b4
Tiago Nunes <ntiagon@gmail.com>
2021-06-24 16:41:39 -0300

    antes de inicar os tokens

---
+++ API_rest/src/controllers/UserController.js
@@ -0,0 +1,82 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const novoUser = await User.create(req.body);
+      return res.json(novoUser);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ error: 'ID não encontrado' });
+
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({ error: 'Usuário não encontrado' });
+      }
+      const newData = user.update(req.body);
+      return res.json(newData);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ error: 'ID não encontrado' });
+
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({ error: 'Usuário não encontrado' });
+      }
+      await user.destroy();
+      return res.json({ deleted: user });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+}
+
+export default new UserController();
---
+++ API_rest/src/models/User.js
@@ -0,0 +1,53 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 100],
+              msg: 'Campo nome tem que ter entre 3 e 100 caracteres.',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: { msg: 'E-mail já existe' },
+          validate: {
+            isEmail: {
+              msg: 'E-mail inválido.',
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [6, 50],
+              msg: 'Campo senha tem que ter entre 6 e 50 caracteres.',
+            },
+          },
+        },
+      },
+      { sequelize }
+    );
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        // eslint-disable-next-line no-param-reassign
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+    return this;
+  }
+}
