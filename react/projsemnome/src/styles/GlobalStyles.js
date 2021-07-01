cdc5559d03512dda0288d3701a2e22e8c03593ce
Tiago Nunes <ntiagon@gmail.com>
2021-07-01 12:32:11 -0300

    globalstyles done

--- react/projsemnome/src/styles/GlobalStyles.js
+++ react/projsemnome/src/styles/GlobalStyles.js
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import { primaryDarkColor, primaryColor } from '../config/colors';
 
 export default createGlobalStyle`
   * {
@@ -10,6 +11,9 @@ export default createGlobalStyle`
 
   body {
     font-family: sans-serif;
+    background:${primaryDarkColor};
+    color: ${primaryColor};
+
   }
   html,
   body,
@@ -18,9 +22,16 @@ export default createGlobalStyle`
   }
   button {
     cursor: pointer;
+    background: ${primaryColor};
+    border: none;
+    color: #fff;
+    padding: 10px 20px;
+    border-radius: 4px;
+    font-weight: 700;
   }
   a {
     text-decoration: none;
+    color: ${primaryColor};
   }
   ul {
     list-style: none;
