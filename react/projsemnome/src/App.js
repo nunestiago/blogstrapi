10f194cd205f56e35e16fa7dbc7ca31bf63eb898
Tiago Nunes <ntiagon@gmail.com>
2021-07-01 13:40:46 -0300

    did private route

--- react/projsemnome/src/App.js
+++ react/projsemnome/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
-import Login from './pages/Login';
+import history from './services/history';
+
 import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </div>
+    </Router>
   );
 }
 
