009041928c61222bb4be9664a4603d7567c1d0bc
Tiago Nunes <ntiagon@gmail.com>
2021-07-01 14:34:59 -0300

    break

--- react/projsemnome/src/pages/Login/index.jsx
+++ react/projsemnome/src/pages/Login/index.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Title } from './styled';
 import { Container } from '../../styles/GlobalStyles';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      console.log(response.data);
+    }
+    getData();
+  }, []);
   return (
-    <>
-      <Container>
-        Hello{' '}
-        <small>
-          <Title>Teste</Title>Oie
-        </small>
-      </Container>
-    </>
+    <Container>
+      Hello{' '}
+      <small>
+        <Title>Teste</Title>Oie
+      </small>
+    </Container>
   );
 }
